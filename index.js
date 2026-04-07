b9344fc44d8a29449ab81a5bcc49cdb263ba636d
pele1610 <pelecolo56@gmail.com>
2026-04-07 22:23:24 +0300

    added two files

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-
-
-
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+    ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+function createLoginTracker(userInfo) {
+    let attemptCount = 0;
+
+    return (passwordAttempt) => {
+        attemptCount++;
+
+
+        if (attemptCount > 3) {
+            return "Account locked due to too many failed login attempts";
+        }
+
+
+        if (passwordAttempt === userInfo.password) {
+            return "Login successful";
+        } else {
+            return `Attempt ${attemptCount}: Login failed`;
+        }
+    };
+}
